15aa0d9fb6b04c79f5d0514737bf7be3936a9771
marceloarmando <mcuellarr98@gmail.com>
2020-06-03 20:03:44 -0500

    commit practica_angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+import { ListadoarticulosComponent } from './listadoarticulos/listadoarticulos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SelectornumericoComponent,
+    ListadoarticulosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
